8ecb87e2709f6857e62b2743d4d10376caa7a7ff
Matt Inkeles <mattinkeles37@gmail.com>
2021-01-21 13:47:01 -0500

    Added proof of concept user view page for admin

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask
+import requests
+
 from scripts.User import User
 
 app = Flask(__name__, static_folder='static')
+
+api_url = 'http://127.0.0.2:5000'
 
 @app.route("/")
 def index():
     user1 = User('regular_user', 'User1')
     return f"Hello, {user1.name}"
 
+@app.route("/users")
+def DisplayAllUsers():
+    response = requests.get(f"{api_url}/users")
+    userList = ''
+    for user in response.json():
+        username = user['username']
+        firstName = user['firstname']
+        lastName = user['lastname']
+        avatarLink = user['avatarlink']
+
+        userList += f"<p>{username}: {firstName}, {lastName}<p>"
+    return f"Users: {userList}"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False)
