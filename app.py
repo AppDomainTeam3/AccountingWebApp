a0191b7bc86a4efeb9affaf802d401a0df9e277d
Daniel <alejosfks@gmail.com>
2021-01-27 11:21:34 -0500

    Added Login Functionality

--- app.py
+++ app.py
@@ -1,17 +1,87 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for,session,flash,g
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import requests
-
 from scripts.User import User
+from scripts.CreateUser import UserCreationForm
+from scripts.LoginUser import UserLoginForm
+import re
+import json
+import flask_login
+import flask
+
 
 app = Flask(__name__, static_folder='static')
+app.config['SECRET_KEY'] = 'secret_key'
 
 app_url = 'https://appdomainteam3.azurewebsites.net'
-api_url = 'https://appdomainteam3api.azurewebsites.net'
+api_url = 'http://127.0.0.2:5000'
 
+response = requests.get(f"{api_url}/users")
+
+class User(UserMixin):
+    def __init__(self,id,username,password):
+        self.id = id
+        self.username = username
+        self.password = password
+    def __repr__(self): 
+        return f'<User: {self.username}>'
+
+dataList = response.json()
+users = []
+#adds id, username, and hashed_password to list for user tracking and authentication
+def update_user_list():
+    for x in range(len(dataList)):
+        dataDict = dataList[x]
+        users.append(User(id=dataDict['id'],username = dataDict['username'],password=dataDict['hashed_password']))
+update_user_list()
+
+#checks if user is logged in
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+
+#starts the session/checks for auth
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    form = UserLoginForm()
+    if request.method == 'POST':
+        session.pop('user_id',None)
+        username = request.form['username']
+        password = request.form['password']
+        try:
+            user = [x for x in users if x.username == username][0]
+        except Exception:
+            return redirect(url_for('login'))
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
+        return redirect(url_for('login'))
+    return render_template('login.html',title = 'Login')
+
+#random page ~ home
+@app.route('/home')
+def home():
+    if not g.user:
+        return '<h1> You are not logged in </h1>'
+    else:
+        return f'''<h1> Welcome: {g.user.username} </h1>
+                   <a href="{url_for('sign_out')}"> Sign Out </a>'''
+
+@app.route('/sign_out')
+def sign_out():
+    session.pop('user_id')
+    return redirect(url_for('login'))
+#index page ~ welcome if signed in / not signed in notification if not
 @app.route("/")
 def index():
-    user = {'username': 'user'}
-    return render_template('index.html', title='Home', user=user)
+    if 'user_id' in session:
+        return f'''<h1>Hello, {g.user.username}</h2>
+                   <a href={url_for('home')}> home page</a>'''
+    return 'You are not signed in!'
 
 @app.route("/users")
 def DisplayAllUsers():
@@ -21,5 +91,13 @@ def DisplayAllUsers():
         userList.append(user)
     return render_template('users.html', title='All Users', userdata=userList)
 
+@app.route("/add-user", methods=['GET', 'POST'])
+def CreateUser():
+    form = UserCreationForm()
+    if (form.validate_on_submit()):
+        response = requests.post(f"{api_url}/users/create-user")
+        return redirect('/users')
+    return render_template('create_user.html', title='Create User', form=form)
+      
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
---
+++ scripts/CreateUser.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+
+class UserCreationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    usertype = SelectField('User Type', choices=[('regular_user', 'Basic'), ('manager', 'Manager'), ('administrator', 'Admin')], validators=[DataRequired()])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    avatarlink = StringField('Avatar Link')
+    submit = SubmitField('Submit')
