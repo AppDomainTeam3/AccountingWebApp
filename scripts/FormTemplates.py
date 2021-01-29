042ce5d0504c5524622d3aba19c1768ed0fb096e
Matt Inkeles <mattinkeles37@gmail.com>
2021-01-29 17:44:04 -0500

    Small fix for last commit

--- scripts/FormTemplates.py
+++ scripts/FormTemplates.py
@@ -3,10 +3,10 @@ from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 class UserCreationForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired('field is required.')])
-    email = StringField('Email', validators=[InputRequired('field is required.')])
-    usertype = SelectField('User Type', choices=[('regular_user', 'Basic'), ('manager', 'Manager'), ('administrator', 'Admin')], validators=[InputRequired('field is required.')])
-    firstname = StringField('First Name', validators=[InputRequired('field is required.')])
-    lastname = StringField('Last Name', validators=[InputRequired('field is required.')])
+    username = StringField('Username', validators=[DataRequired('field is required.')])
+    email = StringField('Email', validators=[DataRequired('field is required.')])
+    usertype = SelectField('User Type', choices=[('regular_user', 'Basic'), ('manager', 'Manager'), ('administrator', 'Admin')], validators=[DataRequired('field is required.')])
+    firstname = StringField('First Name', validators=[DataRequired('field is required.')])
+    lastname = StringField('Last Name', validators=[DataRequired('field is required.')])
     avatarlink = StringField('Avatar Link')
     submit = SubmitField('Submit')
