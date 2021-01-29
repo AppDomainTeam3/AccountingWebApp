3ef199b1ba7094a74113e9dc77d9902680e841ab
Matt Inkeles <mattinkeles37@gmail.com>
2021-01-29 17:41:30 -0500

    Added email field to createuser and updateuser + some cleanup

--- scripts/User.py
+++ scripts/User.py
@@ -1,13 +1,14 @@
-class User:
-    types = ['administrator', 'manager', 'regular_user']
+from flask_login import UserMixin
 
-    type = 'null'
-    name = 'null'
-
-    def __init__(self, user_type, name):
-        if (user_type not in self.types):
-            print(f"User type \'{user_type}\' not found!")
-            return None
-        self.type = user_type
-        self.name = name
-        
+class User(UserMixin):
+    def __init__(self, id, username, email, usertype, firstname, lastname, avatarlink, password):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.usertype = usertype
+        self.firstname = firstname
+        self.lastname = lastname
+        self.avatarlink = avatarlink
+        self.password = password
+    def __repr__(self): 
+        return f'<User: {self.username}>'
