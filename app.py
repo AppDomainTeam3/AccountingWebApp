6c9ad4ed7cc0335f3222c7db203c855fe83fa4a0
Daniel <alejosfks@gmail.com>
2021-01-30 17:23:44 -0500

    update main app to match latest

--- app.py
+++ app.py
@@ -1,40 +1,45 @@
-from flask import Flask, render_template, request, redirect, url_for,session,flash,g
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect, url_for, session, g
+from flask_mail import Mail, Message
+from werkzeug.security import check_password_hash
 import requests
 from scripts.User import User
-from scripts.CreateUser import UserCreationForm
+from scripts.FormTemplates import UserCreationForm
 from scripts.LoginUser import UserLoginForm
-import re
-import json
-import flask_login
-import flask
-
 
 app = Flask(__name__, static_folder='static')
-app.config['SECRET_KEY'] = 'secret_key'
+app.config.from_object("config.DevelopementConfig")
 
-app_url = 'http://127.0.0.1:5000/'
-api_url = 'http://127.0.0.2:5000/'
+app_url = 'https://appdomainteam3.herokuapp.com'
+api_url = 'https://appdomainteam3api.herokuapp.com'
 
-response = requests.get(f"{api_url}/users")
+mail = Mail(app)
 
-class User(UserMixin):
-    def __init__(self,id,username,password):
-        self.id = id
-        self.username = username
-        self.password = password
-    def __repr__(self): 
-        return f'<User: {self.username}>'
-
-dataList = response.json()
+userList = []
 users = []
-#adds id, username, and hashed_password to list for user tracking and authentication
+#adds userdata to list for user tracking and authentication
 def update_user_list():
-    for x in range(len(dataList)):
-        dataDict = dataList[x]
-        users.append(User(id=dataDict['id'],username = dataDict['username'],password=dataDict['hashed_password']))
+    response = requests.get(f"{api_url}/users")
+    userList = response.json()
+    users.clear()
+    for x in range(len(userList)):
+        userDict = userList[x]
+        users.append(User(id=userDict['id'], 
+                          username = userDict['username'],
+                          email = userDict['email'],
+                          usertype = userDict['usertype'],
+                          firstname = userDict['firstname'],
+                          lastname = userDict['lastname'],
+                          avatarlink = userDict['avatarlink'],
+                          password = userDict['hashed_password'],
+                          isactive = userDict['isActive'],
+                          ispasswordexpired = userDict['ispasswordexpired']))
 update_user_list()
+
+def updataUserSessionData():
+    update_user_list()
+    for userIndex in range(len(users)):
+        if g.user.id == users[userIndex].id:
+            g.user = users[userIndex]
 
 #checks if user is logged in
 @app.before_request
@@ -47,6 +52,7 @@ def before_request():
 #starts the session/checks for auth
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
+    form = UserLoginForm()
     if request.method == 'POST':
         session.pop('user_id',None)
         username = request.form['username']
@@ -59,44 +65,92 @@ def login():
             session['user_id'] = user.id
             return redirect(url_for('index'))
         return redirect(url_for('login'))
-    return render_template('login.html', title='Login')
+    return render_template('login.html',title = 'Login', url=app_url)
 
-#random page ~ home
-@app.route('/home')
-def home():
-    if not g.user:
-        return '<h1> You are not logged in TEST HOME/extra PAGE IGNORE</h1>'
+@app.route('/mail')
+def test_mail():
+    if g.user == None:
+        return render_template('login.html')
+    if g.user.usertype == 'administrator':
+        msg = Message('Hello, you have clicked on the link', recipients=['netim11829@alicdh.com'])
+        msg.body = f"Hello {g.user.username}, you are the choosen one"
+        mail.send(msg)   
+        return """<h1>message has been sent</h1>
+            <a href='/'> Back To Main Page</a>"""   
     else:
-        return f'''<h1> Welcome: {g.user.username} </h1>
-                   <a href="{url_for('sign_out')}"> Sign Out </a>'''
+        return '''<h1>You do not have access</h1>
+                  <a href='/'> Back To Main Page</a>'''  
 
 @app.route('/sign_out')
 def sign_out():
+    if g.user == None:
+        return render_template('login.html')
     session.pop('user_id')
     return redirect(url_for('login'))
 #index page ~ welcome if signed in / not signed in notification if not
 @app.route("/")
 def index():
-    if 'user_id' in session:
-        return render_template('index.html',title='home')
-    return """<h1> You are not signed in! </h1>
-              <a href = /login> Login </a>"""
+    if g.user == None:
+        return render_template('login.html')
+    return render_template('index.html', title='home', user=g.user)
 
 @app.route("/users")
 def DisplayAllUsers():
+    if g.user == None:
+        return render_template('login.html')
     response = requests.get(f"{api_url}/users")
     userList = []
     for user in response.json():
         userList.append(user)
-    return render_template('users.html', title='All Users', userdata=userList)
+    return render_template('users.html', title='All Users', userdata=userList, user=g.user, url=app_url)
+
+@app.route("/users/expired_passwords")
+def DisplayAllUsersWithExpiredPasswords():
+    if g.user == None:
+        return render_template('login.html')
+    update_user_list()
+    expiredUsers = []
+    for user_ in users:
+        if users[user_.id].ispasswordexpired == 'True':
+            expiredUsers.append(user_)
+    return render_template('expired_passwords.html', title='Expired Passwords Report', userdata=expiredUsers, user=g.user, url=app_url)
+
+@app.route("/users/<int:user_id>")
+def UserProfile(user_id):
+    if g.user == None:
+        return render_template('login.html')
+    canEdit = False
+    if g.user.usertype == 'administrator' or g.user.id == user_id:
+        canEdit = True
+    response = requests.get(f"{api_url}/users/{user_id}").json()
+    username = response[0]['username']
+    usertype = response[0]['usertype']
+    firstname = response[0]['firstname']
+    lastname = response[0]['lastname']
+    avatarlink = response[0]['avatarlink']
+    updataUserSessionData()
+    return render_template('profile.html', user=g.user, title=username, usertype=usertype, id=user_id, firstname=firstname, lastname=lastname, avatarlink=avatarlink, url=app_url, canEdit=canEdit)
+
+@app.route("/users/<int:user_id>/edit", methods=['GET', 'POST'])
+def EditUserProfile(user_id):
+    if g.user == None:
+        return render_template('login.html')
+    response = requests.get(f"{api_url}/users/{user_id}").json()
+    email = response[0]['email']
+    username = response[0]['username']
+    usertype = response[0]['usertype']
+    firstname = response[0]['firstname']
+    lastname = response[0]['lastname']
+    avatarlink = response[0]['avatarlink']
+    form = UserCreationForm()
+    return render_template('edit_user.html', title=username, form=form, id=user_id, email=email, username=username, usertype=usertype, firstname=firstname, lastname=lastname, avatarlink=avatarlink, url=app_url, api=api_url, user=g.user)
 
 @app.route("/add-user", methods=['GET', 'POST'])
 def CreateUser():
+    if g.user == None:
+        return render_template('login.html')
     form = UserCreationForm()
-    if (form.validate_on_submit()):
-        response = requests.post(f"{api_url}/users/create-user")
-        return redirect('/users')
-    return render_template('create_user.html', title='Create User', form=form)
-      
+    return render_template('create_user.html', title='Create User', form=form, api=api_url, user=g.user)
+
 if __name__ == "__main__":
     app.run(debug=False)
